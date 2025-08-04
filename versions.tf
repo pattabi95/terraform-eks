e4872fdc69c13d9dfda30a79ca334e4e6274fb77
Pattabiraman <pattabimanu@gmail.com>
2025-08-04 19:38:43 +0530

    versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.0"
-        }
+  required_version = ">= 1.5.7, < 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
     }
-    
-    required_version = ">= 0.12"
+  }
 }
