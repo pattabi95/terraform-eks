4fce698ead3bcb35d488510e2ed96f2e71bb098a
Pattabiraman <pattabimanu@gmail.com>
2025-08-04 21:45:57 +0530

    cidr edit

--- variables.tf
+++ variables.tf
@@ -8,15 +8,15 @@ variable "name" {
 }
 variable "cidr" {
     description = "The CIDR block for the VPC."
-    default = "10.0.0/16"
+    default = "10.0.0.0/16"
 }
 variable "public_subnet_cidrs" {
     description = "List of CIDR blocks for public subnets."
-    default = ["10.0.0/24", "10.0.1/24"]
+    default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 variable "private_subnet_cidrs" {
     description = "List of CIDR blocks for private subnets."
-    default = ["10.0.2/24", "10.0.3/24"]
+    default = ["10.0.2.0/24", "10.0.3.0/24"]
 }   
 variable "cluster_name" {
     description = "The name of the Kubernetes cluster."
